d1d46beb3c0c820187a77da7b326a9e11053a53b
stanley <starnleymbote@gmail.com>
2023-11-04 00:23:22 +0300

    able to chnage border color when there is an error

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -21,6 +21,7 @@
 </div>
 
 <div class="form-group mb-4">
+
   <label for="website-admin" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
   <div class="flex">
     <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
@@ -28,16 +29,15 @@
         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
       </svg>
     </span>
-    <input type="text" id="last_name" name="last_name" class="p-3 rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Last Name">
+    <input type="text" id="last_name" name="last_name" class="@error('last_name') border-red-500 @enderror p-3 rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Last Name">
     
-  
-    {{-- <span>
-      @if($errors->has('last_name'))
-      {{ $errors->first('last_name') }}
-    @endif
-    </span> --}}
   </div>
-  <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> Username already taken!</p>
+  @if ($errors->has('last_name'))
+
+      <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $errors->first('last_name') }}</span></p>          
+  
+  @endif
+  
 </div>
 
 <div class="form-group mb-4">
