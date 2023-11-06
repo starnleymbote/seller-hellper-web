3ff8a1206e7d0440a5d30e3e50030354b12e6054
stanley <starnleymbote@gmail.com>
2023-11-07 00:21:19 +0300

    done testing for the user registration

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Roles;
 use App\Providers\RouteServiceProvider;
 
 test('registration screen can be rendered', function () {
@@ -8,14 +10,37 @@ test('registration screen can be rendered', function () {
     $response->assertStatus(200);
 });
 
-test('new users can register', function () {
-    $response = $this->post('/register', [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
-    ]);
+// test('new users can register1', function () {
+//     $response = $this->post('/register', [
+//         'name' => 'Test User',
+//         'email' => 'test@example.com',
+//         'password' => 'password',
+//         'password_confirmation' => 'password',
+//     ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(RouteServiceProvider::HOME);
+//     $this->assertAuthenticated();
+//     $response->assertRedirect(RouteServiceProvider::HOME);
+// });
+
+test('registering a new user', function () {
+
+    $roles = Roles::factory()->create();
+    $role = Roles::select('id')->firstOrFail();
+    $user = new User;
+
+    $user ->first_name = 'John';
+    $user ->last_name = 'Doe';
+    $user ->surname = 'Smith';
+    $user ->phone = '254700000011';
+    $user ->email = 'seller@example.com';
+    $user ->role_id = $role->id; 
+    $user ->uuid = Str::uuid();
+    $user ->password = Hash::make('password');
+
+    $response = $user ->save();
+    $response->toBeTruthy();
+    $this->assertSame(1, User::all()->count());
+    
+
+
 });
--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -10,19 +10,19 @@ use Illuminate\Support\Facades\Hash;
 //     $response->assertStatus(200);
 // });
 
-it('is able to register', function(){
+// it('is able to register', function(){
 
-        $user = new User;
+//         $user = new User;
 
-        $user ->first_name = 'John';
-        $user ->last_name = 'Doe';
-        $user ->surname = 'Smith';
-        $user ->phone = '254700000011';
-        $user ->email = 'seller@example.com';
-        $user ->role_id = 2;
-        $user ->uuid = Str::uuid();
-        $user ->password = Hash::make('password');
+//         $user ->first_name = 'John';
+//         $user ->last_name = 'Doe';
+//         $user ->surname = 'Smith';
+//         $user ->phone = '254700000011';
+//         $user ->email = 'seller@example.com';
+//         $user ->role_id = 2;
+//         $user ->uuid = Str::uuid();
+//         $user ->password = Hash::make('password');
 
-        $response = $user ->save();
-        $response->assertStatus(201);
-});
+//         $response = $user ->save();
+//         $response->assertStatus(201);
+// });
