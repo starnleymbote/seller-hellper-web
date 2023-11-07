7fed4bfa05ca031c7c4e168ba69984d0838fed80
stanley <starnleymbote@gmail.com>
2023-11-07 23:35:27 +0300

    code clean up

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,12 +2,97 @@
 
 use App\Models\User;
 use App\Models\Roles;
+use Illuminate\Http\Request;
+use App\Service\RegistrationService;
 use App\Providers\RouteServiceProvider;
 
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
 
     $response->assertStatus(200);
+});
+
+describe('user registration', function () {
+
+
+        test("register using registration service", function () {
+
+            $roles = Roles::factory()->create();
+            $role = Roles::select('id')->firstOrFail();
+            $registrationService = new RegistrationService();
+            
+            $user = $registrationService->register(new Request([
+                
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'surname' => 'Smith',
+                'phone' => '254700000011',
+                'email' => 'seller@example.com',
+                'role_id' => $role->id,
+                'password' => 'password',
+
+            ]));
+
+        $this->assertSame(1, User::all()->count());
+
+        });
+
+        test("register using valid mail", function () {
+
+            $roles = Roles::factory()->create();
+            $role = Roles::select('id')->firstOrFail();
+            $registrationService = new RegistrationService();
+            
+            $user = $registrationService->register(new Request([
+                
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'surname' => 'Smith',
+                'phone' => '254700000011',
+                'email' => 'seller@example.com',
+                'role_id' => $role->id,
+                'password' => 'password',
+
+            ]));
+
+            
+        $this->assertSame(1, User::all()->count());
+
+        });
+
+        test("register using invalid mail", function () {
+
+            $roles = Roles::factory()->create();
+            $role = Roles::select('id')->firstOrFail();
+            $registrationService = new RegistrationService();
+            
+            $user = $registrationService->register(new Request([
+                
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'surname' => 'Smith',
+                'phone' => '254700000011',
+                'role_id' => $role->id,
+                'password' => 'password',
+
+            ]));
+            
+            expect([
+                
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'surname' => 'Smith',
+                'phone' => '254700000011',
+                'role_id' => $role->id,
+                'password' => 'password',
+
+            ])->not->toContain('email');
+
+        $this->assertSame(0, User::all()->count());
+
+        });
+
+
 });
 
 // test('new users can register1', function () {
@@ -21,26 +106,3 @@ test('registration screen can be rendered', function () {
 //     $this->assertAuthenticated();
 //     $response->assertRedirect(RouteServiceProvider::HOME);
 // });
-
-test('registering a new user', function () {
-
-    $roles = Roles::factory()->create();
-    $role = Roles::select('id')->firstOrFail();
-    $user = new User;
-
-    $user ->first_name = 'John';
-    $user ->last_name = 'Doe';
-    $user ->surname = 'Smith';
-    $user ->phone = '254700000011';
-    $user ->email = 'seller@example.com';
-    $user ->role_id = $role->id; 
-    $user ->uuid = Str::uuid();
-    $user ->password = Hash::make('password');
-
-    $response = $user ->save();
-    $response->toBeTruthy();
-    $this->assertSame(1, User::all()->count());
-    
-
-
-});
