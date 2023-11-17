c3641dcac435bccf130b9a62af2bc26f97e06055
stanley <starnleymbote@gmail.com>
2023-11-18 01:20:28 +0300

    fixing displaying of name in shops list

--- resources/views/shop/user_shop_list.blade.php
+++ resources/views/shop/user_shop_list.blade.php
@@ -34,30 +34,54 @@
             </thead>
             <tbody>
 
-                @foreach ($shops as $shop)
+            
+
+                @if (count($shops) > 0)
+                
+                    @foreach ($shops as $shop)
+                            
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{$shop ->image}}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{$shop ->name}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$shop ->description}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$shop ->description}}
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                            </td>
+                        </tr>
+                        
+                    @endforeach
+                
+                @else
                     
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{$shop ->image}}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{$shop ->name}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$shop ->description}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$shop ->description}}
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                    <tr>
+                        <td class="justify-items-center w-full py-6" colspan="4">
+                            
+                            <div class="grid  justify-items-center">
+                                
+                                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> <span class="text-blue-700"> {{ $owners_name ->first_name }} </span> Has No Shops At The Moment</h5>
+
+                                </a>
+
+                            </div>
+                        
                         </td>
                     </tr>
-                    
-                @endforeach
+                        
+                @endif
                 
             </tbody>
         </table>
